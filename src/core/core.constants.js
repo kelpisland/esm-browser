d1f47a088d64213190ab70178ea71e9a4d1c0085
Shea Phillips <shea.phillips@keystonesystems.ca>
2015-10-08 10:48:18 -0700

    dynamically determine path to mock api server instead of hard-coding localhost.

--- src/core/core.constants.js
+++ src/core/core.constants.js
@@ -3,13 +3,17 @@
 (function() {
     'use strict';
 
+    var apiPath = 'http://' + window.location.hostname + ':7203/api';
+
+    console.debug("API Path is " + apiPath);
+
     angular
         .module('app.core')
         .constant('_', _)
-        .constant('API', 'http://localhost:7203/api')
+        .constant('API',  apiPath)
         .value('Global', {
         					user:{name:{}, _id:undefined},
         					public: true
         				});
-        
+
 })();
