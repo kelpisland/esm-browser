5d132c881740101004c981f6c9c57b7fea8501c3
Ben Leather <ben@3treestech.com>
2015-11-13 21:51:19 -0800

    Added real intake fields and form elements on New Project forms.  Started moving static dropdown data to the constants file.

--- src/core/core.constants.js
+++ src/core/core.constants.js
@@ -6,14 +6,41 @@
     var apiPath = 'http://' + window.location.hostname + ':7203/api';
     var apiServerPath = 'http://159.203.17.90:3000/esm';
 
-    console.debug("API Path is " + apiPath);
-
     angular
         .module('app.core')
         .constant('_', _)
         .constant('API',  apiPath)
         .constant('SERVERAPI',  apiServerPath)
-
+        .constant('PROVINCES', 
+            {
+                'ab': 'Alberta',
+                'bc': 'British Columbia',
+                'mb': 'Manitoba',
+                'nb': 'New Brunswick',
+                'nl': 'Newfoundland and Labrador',
+                'ns': 'Nova Scotia',
+                'on': 'Ontario',
+                'pe': 'Prince Edward Island',
+                'qc': 'Quebec',
+                'sk': 'Saskatchewan',
+                'nt': 'Northwest Territories',
+                'nu': 'Nunavut',
+                'yt': 'Yukon'
+            }
+        )
+        .constant('REGIONS', 
+            {
+                'cariboo': 'Cariboo',
+                'kootenay': 'Kootenay',
+                'lowermainland': 'Lower Mainland',
+                'okanagan': 'Okanagan',
+                'omnieca': 'Omineca',
+                'peace': 'Peace',
+                'skeena': 'Skeena',
+                'thompson': 'Thompson',
+                'vancouverisland': 'Vancouver Island'
+            }
+        )        
         .value('Global', {
 			user:{name:{}, _id:undefined},
 			public: true
